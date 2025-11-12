cc46d845a897ae1a4276683db95ffeb307d7dbbe
tohidshabanloo <tshabanloo@hotmail.com>
2025-11-12 09:19:51 +0330

    fixed the deployment 4

--- vite.config.ts
+++ vite.config.ts
@@ -21,7 +21,17 @@ errorOnDuplicatesPkgDeps(devDependencies, dependencies);
  */
 export default defineConfig(({ command, mode }): UserConfig => {
   return {
-    plugins: [qwikCity(), qwikVite(), tsconfigPaths({ root: "." })],
+    plugins: [
+      qwikCity({
+        staticGenerate: {
+          origin: process.env.VERCEL_URL
+            ? `https://${process.env.VERCEL_URL}`
+            : undefined,
+        },
+      }),
+      qwikVite(),
+      tsconfigPaths({ root: "." }),
+    ],
     // This tells Vite which dependencies to pre-build in dev mode.
     optimizeDeps: {
       // Put problematic deps that break bundling here, mostly those with binaries.
