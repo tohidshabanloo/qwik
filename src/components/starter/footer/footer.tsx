66da2d05aeda390965bee39bbeb7bdaa7e4d4f90
tohidshabanloo <tshabanloo@hotmail.com>
2025-11-12 09:04:19 +0330

    fixed the deployment 2

--- src/components/starter/footer/footer.tsx
+++ src/components/starter/footer/footer.tsx
@@ -1,17 +1,14 @@
 import { component$ } from "@builder.io/qwik";
-import { useServerTimeLoader } from "../../../routes/layout";
 import styles from "./footer.module.css";
 
 export default component$(() => {
-  const serverTime = useServerTimeLoader();
-
   return (
     <footer>
       <div class="container">
         <a href="https://www.Rahvan.ir/" target="_blank" class={styles.anchor}>
           <span>Made with ♡ by Rahvan.ir</span>
           <span class={styles.spacer}>|</span>
-          <span>{serverTime.value.date}</span>
+          <span>{new Date().toISOString()}</span>
         </a>
       </div>
     </footer>
